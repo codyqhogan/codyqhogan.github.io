4558754fc9a327f4b2c0207841da2ebb2e6dbdea
Ismael Castaneda Villegas <ismaelcastaneda.v@gmail.com>
2022-07-18 13:10:27 -0500

    Adding info for database information

--- pdo.php
+++ pdo.php
@@ -1,7 +1,7 @@
 <?php
 
     $pdo = new PDO('mysql:host=sql3.freesqldatabase.com;port=3306;dbname=sql3505436',
-        ${{secrets.DB_USER}}, ${{secrets.DB_PWD}});
+        'sql3505436', 'p9YaPAJQk9');
 
     //throws error if select if wrong
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
